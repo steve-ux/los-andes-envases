5b6fbfc77b8da0e27e7012446b559a68dc0464df
steve-ux <exe.universal@gmail.com>
2022-02-23 14:58:14 -0300

    agregado bootstrap y componente carrusel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
-// import SobreNosotros from "./SobreNosotros";
-// import Contacto from "./Contacto";
+
 
 function Home() {
   return (
-    
-      <div>
-        <h1>Esta es la home</h1>
-        <Link to="/SobreNosotros"><h2>Ver Sobre Nosotros.</h2></Link>
-        <Link to="/Contacto"><h2>Ver Contacto.</h2></Link>
-      </div>
+    <div>
+      <h1 className="titleHome">Los Andes - Evases</h1>
+      <h2>Esta es la home</h2>
+      <Link to="/SobreNosotros">
+        <h2>Ver Sobre Nosotros.</h2>
+      </Link>
+      <Link to="/Contacto">
+        <h2>Ver Contacto.</h2>
+      </Link>
+    </div>
   );
 }
 
