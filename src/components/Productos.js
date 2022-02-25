3dc286cbeba6723cef24b4aab6ce08a40b635a2a
steve-ux <exe.universal@gmail.com>
2022-02-24 23:19:17 -0300

    componente clientes, avance en home, CSS

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -6,148 +6,46 @@ import Producto5 from "../image/5.jpg";
 import Producto6 from "../image/4.jpg";
 import Producto7 from "../image/3.jpg";
 import Producto8 from "../image/_NPH4713.jpg";
-
-
-
+import Producto9 from "../image/_NPH4394.jpg";
 
 function Productos() {
   return (
-    <div class="child-page-listing">
-
-      <div class="grid-container">
-        <article id="3685" class="location-listing">
-          <a class="location-title" href="#">
-            San Francisco{" "}
-          </a>
-
-          <div class="location-image">
-            <a href="#">
-              <img
-              width="600"
-              height="469"
-                src={Producto1}
-                alt="san francisco"
-              />{" "}
-            </a>
-          </div>
-        </article>
-
-        <article id="3688" class="location-listing">
-          <a class="location-title" href="#">
-            London{" "}
-          </a>
-
-          <div class="location-image">
-            <a href="#">
-              <img
-                width="300"
-                height="169"
-                src={Producto2}
-                alt="london"
-              />{" "}
-            </a>
-          </div>
-        </article>
-
-        <article id="3691" class="location-listing">
-          <a class="location-title" href="#">
-            New York{" "}
-          </a>
-
-          <div class="location-image">
-            <a href="#">
-              <img
-                width="300"
-                height="169"
-                src={Producto3}
-                alt="new york"
-              />{" "}
-            </a>
-          </div>
-        </article>
-
-        <article id="3694" class="location-listing">
-          <a class="location-title" href="#">
-            Cape Town{" "}
-          </a>
-
-          <div class="location-image">
-            <a href="#">
-              <img
-                width="300"
-                height="169"
-                src={Producto4}
-                alt="cape town"
-              />{" "}
-            </a>
-          </div>
-        </article>
-
-        <article id="3697" class="location-listing">
-          <a class="location-title" href="#">
-            Beijing{" "}
-          </a>
-
-          <div class="location-image">
-            <a href="#">
-              <img
-                width="300"
-                height="169"
-                src={Producto5}
-                alt="beijing"
-              />{" "}
-            </a>
-          </div>
-        </article>
-
-        <article id="3700" class="location-listing">
-          <a class="location-title" href="#">
-            Paris{" "}
-          </a>
-
-          <div class="location-image">
-            <a href="#">
-              <img
-                width="300"
-                height="169"
-                src={Producto6}
-                alt="paris"
-              />
-            </a>
-          </div>
-        </article>
-        <article id="3700" class="location-listing">
-          <a class="location-title" href="#">
-            Egipto{" "}
-          </a>
-
-          <div class="location-image">
-            <a href="#">
-              <img
-                width="300"
-                height="169"
-                src={Producto7}
-                alt="paris"
-              />
-            </a>
-          </div>
-        </article>
-        <article id="3700" class="location-listing">
-          <a class="location-title" href="#">
-            Mendoza{" "}
-          </a>
-
-          <div class="location-image">
-            <a href="#">
-              <img
-                width="300"
-                height="169"
-                src={Producto8}
-                alt="paris"
-              />
-            </a>
-          </div>
-        </article>
+    <div className="galleryContainer">
+      <div className="galleryItem">
+        <img src={Producto1} alt="" className="galleryImg" />
+        <h2 className="galleryTitle">PERFILES</h2>
+      </div>
+      <div className="galleryItem">
+        <img src={Producto2} alt="" className="galleryImg" />
+        <h2 className="galleryTitle">LÁMINAS</h2>
+      </div>
+      <div className="galleryItem">
+        <img src={Producto3} alt="" className="galleryImg" />
+        <h2 className="galleryTitle">Imagen 3</h2>
+      </div>
+      <div className="galleryItem">
+        <img src={Producto4} alt="" className="galleryImg" />
+        <h2 className="galleryTitle">Imagen 4</h2>
+      </div>
+      <div className="galleryItem">
+        <img src={Producto5} alt="" className="galleryImg" />
+        <h2 className="galleryTitle">CAJÓN ALTO</h2>
+      </div>
+      <div className="galleryItem">
+        <img src={Producto6} alt="" className="galleryImg" />
+        <h2 className="galleryTitle">CAJÓN FRUTA</h2>
+      </div>
+      <div className="galleryItem">
+        <img src={Producto7} alt="" className="galleryImg" />
+        <h2 className="galleryTitle">CAJÓN CORTO</h2>
+      </div>
+      <div className="galleryItem">
+        <img src={Producto8} alt="" className="galleryImg" />
+        <h2 className="galleryTitle">Imagen 8</h2>
+      </div>
+      <div className="galleryItem">
+        <img src={Producto9} alt="" className="galleryImg" />
+        <h2 className="galleryTitle">Imagen 9</h2>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,18 +2,29 @@ import NavBar from "../components/navBar";
 import Carrusel from "../components/Carrusel";
 import ImageCamion from "../image/_NPH48582.jpg";
 import Productos from "../components/Productos";
+import Clientes from "../components/Clientes";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <>
     <NavBar/>
     <Carrusel/>
-    <h3 className="introHome">Somos un aserradero dedicado a la confección de envases a medida para el envasado de productos agrícolas y hortícolas.</h3>
+    <div className="containerIntroHome">
+    <h3 className="introHome">Somos un aserradero dedicado a la <span className="resaltados">confección de envases</span> a medida para el envasado de productos agrícolas y hortícolas.</h3>
+    </div>
     <img src={ImageCamion} className="imageCamion" alt="Camión cargado con cajas"/>
     <h2 className="nuestrosProductos">Nuestros Productos</h2>
     <p className="textNuestrosProductos">Lorem ipsum dolor sit amet consectetuer. Lorem ipsum dolor sit amet consectetuer. Lorem ipsum dolor sit amet consectetuer.Lorem ipsum dolor sit amet consectetuer. Lorem ipsum dolor sit amet consectetuer. Lorem ipsum dolor sit amet consectetuer. Lorem ipsum dolor sit amet consectetuer. Lorem ipsum dolor sit amet consectetuer. Lorem ipsum dolor sit amet consectetuer. Lorem ipsum dolor sit amet consectetuer.</p>
     <div className="productos">
     <Productos/>
+    </div>
+    <h2 className="nuestrosProductos">NUESTROS CLIENTES HABLAN</h2>
+    <div className="clientes">
+    <Clientes/>
+    </div>
+    <div className="btnConocenosContainer">
+    <Link to="/SobreNosotros"><button className="btnConocenos">CONOCENOS</button></Link>
     </div>
     </>
     
